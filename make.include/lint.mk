a52638e039aec00f0a936d480dfbf73566c24cd2
Matt Krueger <mkrueger@rstms.net>
2021-09-12 00:08:00 -0500

    add isort

--- make.include/lint.mk
+++ make.include/lint.mk
@@ -3,8 +3,8 @@ fmt:  ## blacken python source
 	black $(project) tests
 
 lint: fmt  ## check style with flake8
+	isort $(project) tests
 	flake8 $(project) tests
-
 	black --check $(project) tests
 
 # vim:ft=make
